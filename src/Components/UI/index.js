0016d6aeb6bbe5cb2e2f6868e6fbfa333642efdf
Lais Fernandes <laisffn@hotmail.com>
2022-05-30 22:21:14 -0300

    Implements styled-component to box style

--- src/Components/UI/index.js
+++ src/Components/UI/index.js
@@ -4,3 +4,37 @@ export const Icone = styledComponents.img`
   height: 25px;
   width: 25px;
 `;
+
+export const Box = styledComponents.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.04);
+  padding: 20px;
+  width: 48%;
+`;
+
+export const Botao = styledComponents.button`
+  margin: 15px auto 0px auto;
+  display: block;
+  border-radius: 20px;
+  background-color: #41d3be;
+  border: none;
+  color: white;
+  font-weight: 600;
+  font-size: 14px;
+  padding: 8px 20px;
+  cursor: pointer;
+`;
+
+export const Detalhe = styledComponents.span`
+  color: #41d3be;
+  font-size: 24px;
+`;
+
+export const Saldo = styledComponents.div`
+  font-weight: 700;
+  font-size: 32px;
+`;
